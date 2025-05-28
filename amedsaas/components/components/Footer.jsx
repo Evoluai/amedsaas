ba991a4274acbdb58f0cfacd732bbb7744108554
Evoluai <crisrigomoura@gmail.com>
2025-05-28 12:34:08 -0300

    Delete amedsaas/components/components/Footer.jsx
    antigos

--- amedsaas/components/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer style={{ padding: "1rem", marginTop: "2rem", background: "#eee", textAlign: "center" }}>
-    <p>&copy; {new Date().getFullYear()} AmedSaaS - Todos os direitos reservados</p>
-  </footer>
-);
-
-export default Footer;
